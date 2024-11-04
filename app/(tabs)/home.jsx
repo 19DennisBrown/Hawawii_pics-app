43bea40f9577de28575cee74da27d8db136cb47f
19DennisBrown <107542527+19DennisBrown@users.noreply.github.com>
2024-11-04 12:27:28 +0300

    Fetch and render

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,39 @@
 
 
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, ScrollView, FlatList, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
 
 const Home =()=>{
+  const URL = "https://fakestoreapi.com/products";
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const fetchApi=()=>{
+    fetch(URL)
+    .then((response)=>{
+      if(!response.ok){
+        throw new Error("No data found.")
+      }
+      return response.json()
+    })
+    .then((data)=>{
+      setProducts(data)
+      setLoading(false)
+      console.log(data)
+    })
+    .catch((err)=> {
+      setError(err.message)
+      setLoading(false)
+      console.log(err.message)
+    });
+
+  }
+
+  useEffect(()=>{
+    fetchApi()
+  }, [])
   return(
     <SafeAreaView>
       <ScrollView
@@ -17,7 +46,22 @@ const Home =()=>{
         className="grid text-center h-full items-center justify-center"
         >
           <Text className="text-3xl" > {"homepage"} </Text>
+
         </View>
+
+        { loading ? (<ActivityIndicator color={'red'} size={'large'}/>) :
+          error ? <View>{error}</View> :
+         ( <FlatList
+            showsVerticalScrollIndicator={false}
+              data = {products}
+              renderItem={( {item} )=><View>
+                <Image
+                  source={{ uri:item.image }}
+                />
+                <Text> "Kshs." {item.price }</Text>
+              </View>}
+          />)
+        }
      </ScrollView>
    </SafeAreaView>
   )
